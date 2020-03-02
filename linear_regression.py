f5dad2d185979b358a9306207ee8a8fa8f762816
Fredpwol <cyborg290@gmail.com>
2020-03-02 23:44:29 +0100

    updated the variables name

--- linear_regression.py
+++ linear_regression.py
@@ -5,35 +5,35 @@ import numpy as np
 class Linear_Regressor(object):
 
 	def __init__(self):
-		self.weights = None
-		self.biases = None
+		self.weight = None
+		self.bias = None
 
 
 	def train(self, X, y, epochs=50,alpha=0.005,verbose=1):
-		self.weights = 0
-		self.biases = 0
+		self.weight = 0
+		self.bias = 0
 
 		len_nums = len(X)
-		outputs = self.weights * X + self.biases
+		outputs = self.weight * X + self.bias
 		loss = self.cost_function(len_nums,y,outputs)
 
 		for i in range(epochs):
 			if verbose:
 				print(f"Epoch: {i} val loss ======================== {loss}")
 
-			delta_weights = alpha * (np.sum((outputs - y) * X))
+			delta_weight = alpha * (np.sum((outputs - y) * X))
 			delta_bias = alpha * (np.sum(outputs - y))
 
-			self.weights -= delta_weights / len_nums
-			self.biases -= delta_bias / len_nums
+			self.weight -= delta_weight / len_nums
+			self.bias -= delta_bias / len_nums
 
-			outputs = self.weights * X + self.biases
+			outputs = self.weight * X + self.bias
 			loss = self.cost_function(len_nums,y,outputs)
 
 
 	def predict(self,X):
-		assert self.weights != None and self.biases != None
-		outputs = self.weights * X + self.biases
+		assert self.weight != None and self.bias != None
+		outputs = self.weight * X + self.bias
 		return outputs
 
 	@staticmethod
